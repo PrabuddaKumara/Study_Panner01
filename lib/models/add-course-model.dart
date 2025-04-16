cb0d6cda50adac2d547f269dcafc4a970cd8c4de
pasindu <prabudda758@gmail.com>
2025-04-16 19:18:01 +0530

    fromjson dart object convert tojson json object

--- lib/models/add-course-model.dart
+++ lib/models/add-course-model.dart
@@ -1,3 +1,38 @@
-class CourseModel{
-  
+class CourseModel {
+  final String id;
+  final String name;
+  final String description;
+  final String duration;
+  final String schedule;
+  final String instructor;
+
+  CourseModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.duration,
+    required this.schedule,
+    required this.instructor,
+  });
+
+  factory CourseModel.fromJson(Map<String, dynamic> json) {
+    return CourseModel(
+      id: json['id'] ?? '',
+      name: json['name'] ?? '',
+      description: json['description'] ?? '',
+      duration: json['duration'] ?? '',
+      schedule: json['schedule'] ?? '',
+      instructor: json['instructor'] ?? '',
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'duration': duration,
+      'schedule': schedule,
+      'instructor': instructor,
+    };
+  }
 }
