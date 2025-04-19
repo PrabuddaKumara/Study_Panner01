b8c8dde6544c707cd3036dda27c6f9adbeb7b67c
pasindu <prabudda758@gmail.com>
2025-04-19 17:30:00 +0530

    sinle course add

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,4 +1,7 @@
-import 'package:c01/main_page.dart';
+import 'package:c01/models/add-course-model.dart';
+import 'package:c01/pages/add-new-course.dart';
+import 'package:c01/pages/main_page.dart';
+import 'package:c01/pages/single-course.dart';
 import 'package:go_router/go_router.dart';
 
 class Gorouter {
@@ -11,6 +14,24 @@ class Gorouter {
         name: "home",
         builder: (context, state) {
           return MainPage();
+        },
+      ),
+      GoRoute(
+        path: "/add-new-course",
+        name: "add-new-course",
+        builder: (context, state) {
+          return AddNewCourse();
+        },
+      ),
+      GoRoute(
+        path: "/single-course",
+        name: "single-course",
+        builder: (context, state) {
+          final CourseModel course=state.extra as CourseModel;
+
+          return SingleCourse(
+            course: course,
+          );
         },
       ),
     ],
