ea4820a8d57425867883f16c669fbac69d79d79e
pasindu <prabudda758@gmail.com>
2025-04-19 10:18:44 +0530

    snap shot function add

---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final Color greenPrimarycolor=Colors.greenAccent;
+final Color greenNormalcolor=Colors.blueGrey;
+final Color bottomNavigationbarcolor=Colors.grey.withOpacity(0.25);
