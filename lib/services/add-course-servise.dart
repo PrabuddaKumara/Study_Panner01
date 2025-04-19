cf1dfab3a6a612488565281891ec99f9947c62cb
pasindu <prabudda758@gmail.com>
2025-04-19 10:16:14 +0530

    services sream builder

--- lib/services/add-course-servise.dart
+++ lib/services/add-course-servise.dart
@@ -2,29 +2,51 @@ import 'package:c01/models/add-course-model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CourseServises {
-  final CollectionReference coursecollectio = FirebaseFirestore.instance
-      .collection("cousersdf");
+  final CollectionReference courseCollection = FirebaseFirestore.instance
+      .collection("course");
 
-      //add new course
+  //add new course
 
-      Future<void>createNewCourse(CourseModel course)async{
-        try {
-          //conver the courseModel object to map
+  Future<void> createNewCourse(CourseModel course) async {
+    try {
+      final Map<String, dynamic> data = course.toJson();
+      final DocumentReference docfr = await courseCollection.add(data);
+      await docfr.update({'id':docfr.id});
 
-          final Map<String,dynamic>data=course.toJson();
+      //delay navigation to ensure Snackbar is display
 
-          //add the course colllection
+      await Future.delayed(Duration(seconds: 2));
+    } catch (error) {
+      print("Faild the error $error");
+    }
+  }
 
-          await coursecollectio.add(data);
-          
-        } catch (error) {
-          print("Error creating course $course");
-          
-        } 
-      }
+  //get all courses as list  of course
 
-
-
-
+  Stream <List<CourseModel>>get course{
+    try {
+      return courseCollection.snapshots().map(
+        (snapshot){
+          return snapshot.docs.map((doc)=>CourseModel.fromJson(doc.data()as Map<String,dynamic>)).toList();
+        }
+      );
       
+    } catch (error) {
+      print("Fild document $error");
+      return  Stream.empty();
+      
+    }
+  }
+
+  // Stream <List<CourseModel>>get cou{
+  //   try {
+      
+  //   } catch (e) {
+  //     print(object)
+      
+  //   }
+  // }
+
+
+
 }
