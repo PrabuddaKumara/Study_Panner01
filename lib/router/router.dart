e7be35a9b9ba8b5d0b02c0ed6f3aa5694ba86ca7
pasindu <prabudda758@gmail.com>
2025-04-23 05:19:17 +0530

    add new assimt router and note

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,5 +1,7 @@
 import 'package:c01/models/add-course-model.dart';
+import 'package:c01/pages/add-new-assiment.dart';
 import 'package:c01/pages/add-new-course.dart';
+import 'package:c01/pages/add-new-note.dart';
 import 'package:c01/pages/main_page.dart';
 import 'package:c01/pages/single-course.dart';
 import 'package:go_router/go_router.dart';
@@ -27,11 +29,25 @@ class Gorouter {
         path: "/single-course",
         name: "single-course",
         builder: (context, state) {
-          final CourseModel course=state.extra as CourseModel;
+          final CourseModel course = state.extra as CourseModel;
 
-          return SingleCourse(
-            course: course,
-          );
+          return SingleCourse(course: course);
+        },
+      ),
+      GoRoute(
+        path: "/add-new-assiment",
+        name: "add-new-assiment",
+        builder: (context, state) {
+          final CourseModel cou = state.extra as CourseModel;
+          return AddNewAssiment(course: cou);
+        },
+      ),
+      GoRoute(
+        path: "/add-new-note",
+        name: "add-new-note",
+        builder: (context, state) {
+          final CourseModel cour = state.extra as CourseModel;
+          return AddNewNotes(course: cour);
         },
       ),
     ],
