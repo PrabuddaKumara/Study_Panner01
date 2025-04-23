c14dd9231580cb26400ab46efd01a017917085a5
pasindu <prabudda758@gmail.com>
2025-04-23 05:32:39 +0530

    add widgets services

---
+++ lib/widgets/services/font-style-services.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppWidgets {
+  static TextStyle HeadLineStyle() {
+    return TextStyle(color: const Color.fromARGB(255, 221, 206, 206),fontSize: 20);
+    
+  }
+}
