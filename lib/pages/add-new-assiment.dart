3e2e1406beb8b8fb09adc2e305d0ee0385883151
pasindu <prabudda758@gmail.com>
2025-04-24 06:47:22 +0530

    add Date and time used valueListernabuilder

--- lib/pages/add-new-assiment.dart
+++ lib/pages/add-new-assiment.dart
@@ -4,75 +4,147 @@ import 'package:c01/widgets/custom-input.dart';
 import 'package:flutter/material.dart';
 
 class AddNewAssiment extends StatelessWidget {
-  final _forkey = GlobalKey<FormState>();
-
+  final CourseModel course;
+  final _formkey = GlobalKey<FormState>();
   final TextEditingController _assimentNameController = TextEditingController();
   final TextEditingController _assimentDescriptionController =
       TextEditingController();
   final TextEditingController _assimentDuractionController =
       TextEditingController();
+  final ValueNotifier<DateTime> _selectDate = ValueNotifier<DateTime>(
+    DateTime.now(),
+  );
+  final ValueNotifier<TimeOfDay> _selectTime = ValueNotifier<TimeOfDay>(
+    TimeOfDay.now(),
+  );
 
-  final CourseModel course;
-  //submit form
-
-  void _subminform(BuildContext context) async {
-    if (_forkey.currentState!.validate() ?? false) {}
+  AddNewAssiment({super.key, required this.course}) {
+    _selectDate.value = DateTime.now();
+    _selectTime.value = TimeOfDay.now();
   }
 
-  AddNewAssiment({super.key, required this.course});
+  //Date Picker
+
+  Future<void> _selecteddate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      firstDate: DateTime(2023),
+      lastDate: DateTime(2026),
+      initialDate: _selectDate.value,
+    );
+    if (picked != null && picked != _selectDate.value) {
+      _selectDate.value = picked;
+    }
+  }
+  //Time Picker
+
+  Future<void> _selectedTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: _selectTime.value,
+    );
+    if (picked != null && picked != _selectTime.value) {
+      _selectTime.value = picked;
+    }
+  }
+
+  void _submitform(BuildContext context) async {
+    if (_formkey.currentState!.validate() ?? false) {}
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
-          child: Form(
-            key: _forkey,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+
+        child: Form(
+          key: _formkey,
+          child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text("Add New Assiment"),
+                SizedBox(height: 20),
+                Text(
+                  "The overall goal of this workshop is to bring researchers, academicians,  that make the best of the limited data in the medical domain",
+                ),
+                SizedBox(height: 20),
                 CustomInput(
                   controllered: _assimentNameController,
-                  textName: "Assimnet Name",
+                  textName: "Assiment Name",
                   validetor: (value) {
                     if (value?.isEmpty ?? true) {
-                      return "Please Error Name";
+                      return "Please Enter  the Name";
                     }
                     return null;
                   },
                 ),
                 CustomInput(
-                  controllered: _assimentNameController,
-                  textName: "Assimnet Description",
+                  controllered: _assimentDescriptionController,
+                  textName: "Assiment Description",
                   validetor: (value) {
                     if (value?.isEmpty ?? true) {
-                      return "Please Error Description";
+                      return "Please Enter  the Description";
                     }
                     return null;
                   },
                 ),
                 CustomInput(
-                  controllered: _assimentNameController,
-                  textName: "Assimnet Duraction",
+                  controllered: _assimentDuractionController,
+                  textName: "Assiment Duraction",
                   validetor: (value) {
                     if (value?.isEmpty ?? true) {
-                      return "Please Error Duraction";
+                      return "Please Enter  the Description";
                     }
                     return null;
                   },
                 ),
                 SizedBox(height: 20),
-                Divider(thickness: 5.0),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ButtonWidget(
-                    textName: "Add Assiment",
-                    onpresed: ()=>_subminform(context),
-                  ),
+
+                SizedBox(height: 20),
+                Divider(),
+                SizedBox(height: 20),
+                Text("Due Date and Time"),
+                SizedBox(height: 5),
+                ValueListenableBuilder<DateTime>(
+                  valueListenable: _selectDate,
+                  builder: (context, data, child) {
+                    return Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            "Date : ${data.toLocal().toString().split(" ")[0]}",
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () => _selecteddate(context),
+                          icon: Icon(Icons.calendar_today),
+                        ),
+                      ],
+                    );
+                  },
                 ),
+                ValueListenableBuilder<TimeOfDay>(
+                  valueListenable: _selectTime,
+                  builder: (context, time, child) {
+                    return Row(
+                      children: [
+                        Expanded(child: Text("Time : ${time.format(context)}")),
+                        IconButton(
+                          onPressed: () => _selectedTime(context),
+                          icon: Icon(Icons.access_time),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                ButtonWidget(
+                  textName: "Add Assiment",
+                  onpresed: () => _submitform(context),
+                ),
+                SizedBox(height: 20),
               ],
             ),
           ),
